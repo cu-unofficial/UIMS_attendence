d0767101a467aa6af7b09e6a389dd0dd914adfeb
Souvik <souvikde.ns@gmail.com>
2020-05-18 11:27:54 +0530

    shift to electron

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, Tray, Menu, BrowserWindow } = require('electron')
+const path = require('path')
+
+
+const iconpath = path.join(__dirname, 'ninja.png')
+let appIcon = null
+let win = null 
+
+app.on('ready',()=> {
+    win = new BrowserWindow({show: false})
+    appIcon = new Tray(iconpath)
+    var contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Exit',
+            type: 'normal',
+            click: () => {
+                process.exit()
+            }
+        }
+    ])
+    appIcon.setContextMenu(contextMenu)
+    appIcon.on('click',()=>{
+        let mainWindow = new BrowserWindow({
+            show: true
+        })
+
+        mainWindow.loadFile(path.join(__dirname,'src','views','index.html'))
+        const customMenu = Menu.buildFromTemplate([
+            {label: 'Login',type: "normal", click: () => {
+                console.log('Login Process')
+            }}
+        ])
+        Menu.setApplicationMenu(customMenu)
+    })
+})
